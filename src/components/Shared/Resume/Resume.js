d6104d2f7b24288bcab58a4070818cb0e9e4569d
developer-Azad <akalamazad500@gmail.com>
2021-11-29 23:44:44 +0600

    navbar

---
+++ src/components/Shared/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h2>Thi is my resume</h2>
+            <button></button>
+        </div>
+    );
+};
+
+export default Resume;
